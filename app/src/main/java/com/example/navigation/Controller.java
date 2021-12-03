edf1b598b78f7c7c2c0205cb1575a966e73051a6
yashgoel910 <ygoel910@gmail.com>
2021-12-02 19:50:25 -0500

    adding all routes

--- app/src/main/java/com/example/navigation/Controller.java
+++ app/src/main/java/com/example/navigation/Controller.java
@@ -9,12 +9,14 @@ public class Controller extends Activity {
 public String  getObject(String id){
     //Log.d("Object ID is :", String.valueOf(id));
     switch(id){
-        case "33" : return "33";
+        case "12" : return "12";
+        case "13" : return "13";
         case "21" : return "21";
+        case "22" : return "22";
+        case "23" : return "23";
         case "31" : return "31";
         case "32" : return "32";
-        case "12" : return "12";
-        case "22" : return "22";
+        case "33" : return "33";
         case "reset" : return "reset";
         default: return "";
     }
--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     String response = "";
     String flag = "0";
     String step = "0";
+    String destination = "0";
+    String done = "0";
     MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         if(view.getId() == R.id.reset){
             response = controller.getObject("reset");
         }
-        if(flag == "0"){
+        if(flag != "2"){
         switch (view.getId()){
             case R.id.b11: response = controller.getObject("11"); break;
             case R.id.b12: response = controller.getObject("12"); break;
@@ -82,33 +84,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
             }
         }
-        else if(flag == "1" && step == "1"){
-            switch(view.getId()){
-                case R.id.b21: response = controller.getObject("21"); break; //route 1
-                case R.id.b12: response = controller.getObject("12"); break; //route 2
-                case R.id.reset: response = controller.getObject("reset");break;
-                default: Toast.makeText(this, "This action is not handled",
-                        Toast.LENGTH_SHORT).show();
+        else if (flag == "2"){
+            if(view.getId() == R.id.reset){
+                response = controller.getObject("reset");
             }
-        }
-        else if(flag == "1" && step == "2"){
-            switch(view.getId()){
-                case R.id.b31: response = controller.getObject("31"); break;
-                case R.id.b22: response = controller.getObject("22"); break;
-                case R.id.reset: response = controller.getObject("reset");break;
-                default: Toast.makeText(this, "This action is not handled",
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        else if(flag == "1" && step == "3"){
-            if(view.getId() == R.id.b32){
-                response = controller.getObject("32");
-            }
-        }
-        else if(flag == "1" && step == "4"){
-            if(view.getId() == R.id.b33){
-                response = controller.getObject("33");
+            else {
+                Toast.makeText(this, "Please reset the route", Toast.LENGTH_SHORT).show();
             }
         }
         displayRoute(response);
@@ -136,21 +117,276 @@ public class MainActivity extends AppCompatActivity {
         line31_32.setVisibility(View.INVISIBLE);
         line32_33.setVisibility(View.INVISIBLE);
     }
+    public void arrived(){
+        mp.start();
+        Toast.makeText(this,"You have arrived!!",Toast.LENGTH_SHORT).show();
+        step ="0";
+        flag ="2";
+    }
 
     public void displayRoute(String id){
         switch(id){
-            case "reset": {
-                disableLights();
-                disablePaths();
-                flag = "0";
-                step = "0";
-                b33.setImageDrawable(getDrawable(R.drawable.basket));
+            case "12": {
+                //12 is the destination
+                if(flag == "0" && step == "0"){
+                    disableLights();
+                    destination = "12";
+                    flag = "1";
+                    step = "1";
+                    disableLights();
+                    g11.setVisibility(View.VISIBLE);
+                    line11_12.setVisibility(View.VISIBLE);
+                    done = "1";
+                }
+                else if(flag == "1" && step == "1"){
+                    if(destination == "12"){
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        arrived();
+                    }
+                    else if (destination == "13"){
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        line12_13.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "22"){
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        line12_22.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "23"){
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        line12_13.setVisibility(View.VISIBLE);
+                        line13_23.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "32"){
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        g12.setVisibility(View.VISIBLE);
+                        line12_22.setVisibility(View.VISIBLE);
+                        line22_32.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "33") {
+                        disablePaths();
+                        disableLights();
+                        g12.setVisibility(View.VISIBLE);
+                        g12.setVisibility(View.VISIBLE);
+                        line12_22.setVisibility(View.VISIBLE);
+                        line22_32.setVisibility(View.VISIBLE);
+                        line32_33.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                }
+            }break;
+            case "13": {
+                //13 is the destination
+                if(flag == "0" && step == "0"){
+                    destination = "13";
+                    flag = "1";
+                    step = "1";
+                    disableLights();
+                    g11.setVisibility(View.VISIBLE);
+                    line11_12.setVisibility(View.VISIBLE);
+                    line12_13.setVisibility(View.VISIBLE);
+                }
+                else if(flag == "1" && destination == "13"){
+                    disablePaths();
+                    disableLights();
+                    g13.setVisibility(View.VISIBLE);
+                    arrived();
+                }
+                else if(flag == "1" && destination == "23"){
+                    disablePaths();
+                    disableLights();
+                    g13.setVisibility(View.VISIBLE);
+                    line13_23.setVisibility(View.VISIBLE);
+                    step = "3";
+                }
+            }break;
+            case "21": {
+
+                if (flag == "0" && step =="0"){
+                    disableLights();
+                    destination = "21";
+                    flag = "1";
+                    step = "1";
+                    g11.setVisibility(View.VISIBLE);
+                    line11_21.setVisibility(View.VISIBLE);
+                }
+                else if(flag == "1" && step == "1"){
+                    if(destination == "21"){
+                        disablePaths();
+                        disableLights();
+                        g21.setVisibility(View.VISIBLE);
+                        arrived();
+                    }
+                    else if(destination == "22"){
+                        Toast.makeText(this,"No route from here",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(destination == "13"){
+                        Toast.makeText(this,"No route from here",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(destination == "23"){
+                        Toast.makeText(this,"No route from here",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(destination == "31"){
+                        disablePaths();
+                        disableLights();
+                        g21.setVisibility(View.VISIBLE);
+                        line21_31.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "32"){
+                        disablePaths();
+                        disableLights();
+                        g21.setVisibility(View.VISIBLE);
+                        line21_31.setVisibility(View.VISIBLE);
+                        line31_32.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                    else if(destination == "33") {
+                        disableLights();
+                        disablePaths();
+                        g21.setVisibility(View.VISIBLE);
+                        line21_31.setVisibility(View.VISIBLE);
+                        line31_32.setVisibility(View.VISIBLE);
+                        line32_33.setVisibility(View.VISIBLE);
+                        step = "2";
+                    }
+                }
+
+            }break;
+            case "22": {
+                if (flag == "0" && step =="0"){
+                    disableLights();
+                    destination = "22";
+                    flag = "1";
+                    step = "1";
+                    g11.setVisibility(View.VISIBLE);
+                    line11_12.setVisibility(View.VISIBLE);
+                    line12_22.setVisibility(View.VISIBLE);
+                }
+                else if(flag == "1" && step == "2"){
+                    if(destination == "22"){
+                        disableLights();
+                        disablePaths();
+                        g22.setVisibility(View.VISIBLE);
+                        arrived();
+                    }
+                    else if(destination == "23"){
+                        Toast.makeText(this,"No route from here",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(destination == "32"){
+                        disableLights();
+                        disablePaths();
+                        g22.setVisibility(View.VISIBLE);
+                        line22_32.setVisibility(View.VISIBLE);
+                        step = "3";
+                    }
+                    else if(destination == "33") {
+                        disableLights();
+                        disablePaths();
+                        g22.setVisibility(View.VISIBLE);
+                        line22_32.setVisibility(View.VISIBLE);
+                        line32_33.setVisibility(View.VISIBLE);
+                        step = "3";
+                    }
+                }
+            }break;
+            case "23" :{
+                if (flag == "0" && step =="0") {
+                    disableLights();
+                    destination = "23";
+                    flag = "1";
+                    step = "1";
+                    g11.setVisibility(View.VISIBLE);
+                    line11_12.setVisibility(View.VISIBLE);
+                    line12_13.setVisibility(View.VISIBLE);
+                    line13_23.setVisibility(View.VISIBLE);
+                }
+                else if ( flag == "1" && destination == "23"){
+                    disableLights();
+                    disablePaths();
+                    g23.setVisibility(View.VISIBLE);
+                    arrived();
+                }
+
+            }  break;
+            case "31": {
+                if (flag == "0" && step =="0") {
+                    disableLights();
+                    destination = "31";
+                    flag = "1";
+                    step = "1";
+                    g11.setVisibility(View.VISIBLE);
+                    line11_21.setVisibility(View.VISIBLE);
+                    line21_31.setVisibility(View.VISIBLE);
+                }
+                else if(flag == "1" && step == "2"){
+                    if(destination == "31"){
+                        disableLights();
+                        disablePaths();
+                        arrived();
+                        g31.setVisibility(View.VISIBLE);
+                    }
+                    else if(destination == "32"){
+                        disableLights();
+                        disablePaths();
+                        g31.setVisibility(View.VISIBLE);
+                        line31_32.setVisibility(View.VISIBLE);
+                        step = "3";
+                    }
+                    else if(destination == "33") {
+                        disableLights();
+                        disablePaths();
+                        g31.setVisibility(View.VISIBLE);
+                        line31_32.setVisibility(View.VISIBLE);
+                        line32_33.setVisibility(View.VISIBLE);
+                        step = "3";
+                    }
+                }
+                else if(flag == "0"){Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();}
+            }break;
+            case "32": {
+                if (flag == "0" && step =="0") {
+                    disableLights();
+                    destination = "32";
+                    flag = "1";
+                    step = "1";
+                    g11.setVisibility(View.VISIBLE);
+                    line11_21.setVisibility(View.VISIBLE);
+                    line21_31.setVisibility(View.VISIBLE);
+                    line31_32.setVisibility(View.VISIBLE);
+                }
+                else if(flag == "1" && step == "3" && destination == "33"){
+                    disablePaths();
+                    disableLights();
+                    g32.setVisibility(View.VISIBLE);
+                    line32_33.setVisibility(View.VISIBLE);
+                    step ="4";
+                }
+                else if(flag == "1" && destination == "32"){
+                    disablePaths();
+                    disableLights();
+                    g32.setVisibility(View.VISIBLE);
+                    arrived();
+                }
             }break;
             case "33": {
-                disableLights();
                 if(flag == "0") {
                     flag = "1";
                     step = "1";
+                    destination = "33";
                     disableLights();
                     g11.setVisibility(View.VISIBLE);
                     line11_21.setVisibility(View.VISIBLE);
@@ -158,77 +394,21 @@ public class MainActivity extends AppCompatActivity {
                     line31_32.setVisibility(View.VISIBLE);
                     line32_33.setVisibility(View.VISIBLE);
                 }
-                else if (flag == "1" && step =="4"){
+                else if (flag == "1" && step =="4" && destination == "33"){
                     disableLights();
-                    mp.start();
                     g32.setVisibility(View.INVISIBLE);
                     g33.setVisibility(View.VISIBLE);
                     line32_33.setVisibility(View.INVISIBLE);
-                    b33.setImageDrawable(getDrawable(R.drawable.greenbucket));
-                    Toast.makeText(this,"You have arrived!!",Toast.LENGTH_SHORT).show();
-                    step ="0";
-                    flag ="2";
-                }
-                else if(flag == "2"){
-                    Toast.makeText(this,"Please reset the route",Toast.LENGTH_SHORT).show();
+                    arrived();
                 }
             } break;
-            case "21": {
+            case "reset": {
                 disableLights();
-                if(flag == "1" && step == "1"){
-                    g11.setVisibility(View.INVISIBLE);
-                    g21.setVisibility(View.VISIBLE);
-                    line11_21.setVisibility(View.INVISIBLE);
-                    step ="2";
-                }
-                else if(flag == "0"){Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();}
-            }break;
-            case "12": {
-                disableLights();
-                if(flag == "1" && step == "1"){
-                    g11.setVisibility(View.INVISIBLE);
-                    g12.setVisibility(View.VISIBLE);
-                    line11_21.setVisibility(View.INVISIBLE);
-                    line21_31.setVisibility(View.INVISIBLE);
-                    line31_32.setVisibility(View.INVISIBLE);
-                    line32_33.setVisibility(View.INVISIBLE);
-                    line12_22.setVisibility(View.VISIBLE);
-                    line22_32.setVisibility(View.VISIBLE);
-                    line32_33.setVisibility(View.VISIBLE);
-                    step ="2";
-                }
-                else if(flag == "0"){Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();}
-            }break;
-            case "31": {
-                disableLights();
-                if(flag == "1" && step == "2"){
-                    g21.setVisibility(View.INVISIBLE);
-                    g31.setVisibility(View.VISIBLE);
-                    line21_31.setVisibility(View.INVISIBLE);
-                    step ="3";
-                }
-                else if(flag == "0"){Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();}
-            }break;
-            case "32": {
-                disableLights();
-                if(flag == "1" && step == "3"){
-                    g31.setVisibility(View.INVISIBLE);
-                    g22.setVisibility(View.INVISIBLE);
-                    g32.setVisibility(View.VISIBLE);
-                    line31_32.setVisibility(View.INVISIBLE);
-                    line22_32.setVisibility(View.INVISIBLE);
-                    step ="4";
-                }
-                else if(flag == "0"){Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();}
-            }break;
-            case "22":{
-                disableLights();
-                if(flag == "1" && step == "2"){
-                    g12.setVisibility(View.INVISIBLE);
-                    g22.setVisibility(View.VISIBLE);
-                    line12_22.setVisibility(View.INVISIBLE);
-                    step ="3";
-                }
+                disablePaths();
+                flag = "0";
+                step = "0";
+                String destination = "0";
+                String done = "0";
             }break;
             default: Toast.makeText(this,"Route not available yet "+ id,Toast.LENGTH_SHORT).show();
 
